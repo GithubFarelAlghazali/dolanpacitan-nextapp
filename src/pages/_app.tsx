464c5b28b472b7f258afbfbc5adc96e8aeb8369c
GithubFarelAlghazali <farelgithub@gmail.com>
2025-07-10 14:33:50 +0700

    add google map and bottom section in destination detail page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,10 +5,10 @@ import Footer from "./parts/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
      return (
-          <>
+          <div className="h-fit">
                <Navbar />
                <Component {...pageProps} className="h-full" />;
                <Footer />
-          </>
+          </div>
      );
 }
--- src/pages/destination/[id].tsx
+++ src/pages/destination/[id].tsx
@@ -1,5 +1,6 @@
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { TicketIcon, LocateIcon } from "../../../public/icons/icons";
 
 const DetailDestinationPage = () => {
      const { query } = useRouter();
@@ -14,12 +15,13 @@ const DetailDestinationPage = () => {
      });
 
      return (
-          <main className="bg-purple-200 w-screen p-18 md:p-32 flex flex-col md:flex-row gap-8" id="about">
+          <main className="bg-purple-200 w-screen p-18 md:p-32 flex flex-col lg:grid grid-cols-2 grid-rows-2 gap-8">
                <div className="md:w-full">
-                    <h2 className="font-semibold text-4xl mb-12">{data.title}</h2>
+                    <h1 className="font-semibold text-4xl mb-12">{data.title}</h1>
                     <img src={`/images/destinations/${data.img}`} alt="" className="w-full rounded-md" />
                </div>
-               <article className="flex flex-col gap-3 md:w-full md:mt-32 text-sm md:text-base">
+               <article className="flex flex-col gap-3 md:w-full md:mt-20 text-sm md:text-base">
+                    <h3 className="font-semibold text-2xl mb-4">Tentang {data.title}</h3>
                     <p>Kabupaten Pacitan adalah sebuah kabupaten di barat daya provinsi Jawa Timur, berbatasan langsung dengan kabupaten Wonogiri, Jawa Tengah di sebelah barat, & samudera Hindia di sebelah selatan.</p>
                     <p>
                          Pacitan juga dikenal memiliki gua-gua yang indah, diantaranya Gua Gong, Gua Tabuhan (batu dapat dipukul dan berbunyi seperti alat musik gamelan), Gua Kalak (gua pertapaan), dan Gua Luweng Jaran (diduga sebagai kompleks gua
@@ -27,6 +29,22 @@ const DetailDestinationPage = () => {
                          Susilo Bambang Yudhoyono.
                     </p>
                </article>
+               <section className="col-span-2">
+                    <iframe
+                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15794.507524216038!2d110.97260023301688!3d-8.240221882371037!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7bdde98e5438dd%3A0x61e796fc381a11db!2sPantai%20Kasap!5e0!3m2!1sid!2sid!4v1752130488242!5m2!1sid!2sid"
+                         className="w-full h-56 lg:h-full rounded-md"
+                         loading="lazy"
+                         referrerPolicy="no-referrer-when-downgrade"
+                    ></iframe>
+                    <ul className="*:flex flex flex-col md:flex-row my-10 bg-purple-900 text-white p-5 rounded-md  gap-4 lg:gap-10 *:w-full *:gap-2">
+                         <li>
+                              <LocateIcon /> Gumuharjo, Watukarung, Kec. Pringkuku, Kabupaten Pacitan, Jawa Timur
+                         </li>
+                         <li>
+                              <TicketIcon /> Rp7.000/org
+                         </li>
+                    </ul>
+               </section>
           </main>
      );
 };
