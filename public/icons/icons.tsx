e1c389639c8d25478f97cf958f62e07356bd3703
GithubFarelAlghazali <farelgithub@gmail.com>
2025-07-13 21:44:31 +0700

    Detail Page: enable SSR, use Next Image, and add skeleton layout

--- public/icons/icons.tsx
+++ public/icons/icons.tsx
@@ -4,7 +4,7 @@ type Props = {
 
 export const DetailIcon = (props: Props) => {
      return (
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3" className={`${props.className} fill-current`}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#e3e3e3" className={`${props.className} fill-current size-5`}>
                <path d="m298-262-56-56 121-122H80v-80h283L242-642l56-56 218 218-218 218Zm222-18v-80h360v80H520Zm0-320v-80h360v80H520Zm120 160v-80h240v80H640Z" />
           </svg>
      );
@@ -12,15 +12,15 @@ export const DetailIcon = (props: Props) => {
 
 export const LocateIcon = (props: Props) => {
      return (
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" height="24px" width="24px" fill="#e3e3e3" className={`${props.className} fill-current`}>
-               <path d="M480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-180q45-45 80-93 30-41 55-90t25-97q0-66-47-113t-113-47q-66 0-113 47t-47 113q0 48 25 97t55 90q35 48 80 93Zm0-220q-25 0-42.5-17.5T420-540q0-25 17.5-42.5T480-600q25 0 42.5 17.5T540-540q0 25-17.5 42.5T480-480Z" />
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#e3e3e3" className={`${props.className} fill-current w-10`}>
+               <path d="m600-120-240-84-186 72q-20 8-37-4.5T120-170v-560q0-13 7.5-23t20.5-15l212-72 240 84 186-72q20-8 37 4.5t17 33.5v560q0 13-7.5 23T812-192l-212 72Zm-40-98v-468l-160-56v468l160 56Zm80 0 120-40v-474l-120 46v468Zm-440-10 120-46v-468l-120 40v474Zm440-458v468-468Zm-320-56v468-468Z" />
           </svg>
      );
 };
 
 export const TicketIcon = (props: Props) => {
      return (
-          <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3" className={`${props.className} fill-current`}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="#e3e3e3" className={`${props.className} fill-current w-10`}>
                <path d="M480-280q17 0 28.5-11.5T520-320q0-17-11.5-28.5T480-360q-17 0-28.5 11.5T440-320q0 17 11.5 28.5T480-280Zm0-160q17 0 28.5-11.5T520-480q0-17-11.5-28.5T480-520q-17 0-28.5 11.5T440-480q0 17 11.5 28.5T480-440Zm0-160q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm320 440H160q-33 0-56.5-23.5T80-240v-160q33 0 56.5-23.5T160-480q0-33-23.5-56.5T80-560v-160q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v160q-33 0-56.5 23.5T800-480q0 33 23.5 56.5T880-400v160q0 33-23.5 56.5T800-160Zm0-80v-102q-37-22-58.5-58.5T720-480q0-43 21.5-79.5T800-618v-102H160v102q37 22 58.5 58.5T240-480q0 43-21.5 79.5T160-342v102h640ZM480-480Z" />
           </svg>
      );
--- src/pages/destination/[id].tsx
+++ src/pages/destination/[id].tsx
@@ -1,51 +1,90 @@
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
 import { TicketIcon, LocateIcon } from "../../../public/icons/icons";
+import { GetServerSideProps } from "next";
+import Image from "next/image";
 
-const DetailDestinationPage = () => {
-     const { query } = useRouter();
-     const [data, setData] = useState([]);
+type DestinationType = {
+     id: string;
+     title: string;
+     img: string;
+     location?: string;
+     price?: string;
+};
 
-     useEffect(() => {
-          fetch("/api/destination/" + query.id)
-               .then((response) => response.json())
-               .then((data) => {
-                    setData(data.data);
-               });
-     });
+type DetailProps = {
+     data: DestinationType;
+};
 
+export const getServerSideProps: GetServerSideProps = async (context) => {
+     const { id } = context.params!;
+
+     const protocol = context.req.headers.host?.startsWith("localhost") ? "http" : "https";
+     const baseUrl = `${protocol}://${context.req.headers.host}`;
+
+     const res = await fetch(`${baseUrl}/api/destination/${id}`);
+     const json = await res.json();
+
+     if (!json.status) {
+          return { notFound: true };
+     }
+
+     return {
+          props: {
+               data: json.data,
+          },
+     };
+};
+
+const DetailDestinationPage = ({ data }: DetailProps) => {
      return (
-          <main className="bg-purple-200 w-screen p-18 md:p-32 flex flex-col lg:grid grid-cols-2 grid-rows-2 gap-8">
-               <div className="md:w-full">
-                    <h1 className="font-semibold text-4xl mb-12">{data.title}</h1>
-                    <img src={`/images/destinations/${data.img}`} alt="" className="w-full rounded-md" />
-               </div>
-               <article className="flex flex-col gap-3 md:w-full md:mt-20 text-sm md:text-base">
-                    <h3 className="font-semibold text-2xl mb-4">Tentang {data.title}</h3>
-                    <p>Kabupaten Pacitan adalah sebuah kabupaten di barat daya provinsi Jawa Timur, berbatasan langsung dengan kabupaten Wonogiri, Jawa Tengah di sebelah barat, & samudera Hindia di sebelah selatan.</p>
-                    <p>
-                         Pacitan juga dikenal memiliki gua-gua yang indah, diantaranya Gua Gong, Gua Tabuhan (batu dapat dipukul dan berbunyi seperti alat musik gamelan), Gua Kalak (gua pertapaan), dan Gua Luweng Jaran (diduga sebagai kompleks gua
-                         terluas di Asia Tenggara). Pacitan juga memiliki banyak pantai indah, seperti Pantai Klayar, Pantai Srau, Pantai Teleng Ria, dan Pantai Kasap. Selain itu, Pacitan juga dikenal sebagai kota kelahiran Presiden Indonesia ke-6,
-                         Susilo Bambang Yudhoyono.
-                    </p>
-               </article>
-               <section className="col-span-2">
-                    <iframe
-                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15794.507524216038!2d110.97260023301688!3d-8.240221882371037!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7bdde98e5438dd%3A0x61e796fc381a11db!2sPantai%20Kasap!5e0!3m2!1sid!2sid!4v1752130488242!5m2!1sid!2sid"
-                         className="w-full h-56 lg:h-full rounded-md"
-                         loading="lazy"
-                         referrerPolicy="no-referrer-when-downgrade"
-                    ></iframe>
-                    <ul className="*:flex flex flex-col md:flex-row my-10 bg-purple-900 text-white p-5 rounded-md  gap-4 lg:gap-10 *:w-full *:gap-2">
-                         <li>
-                              <LocateIcon /> Gumuharjo, Watukarung, Kec. Pringkuku, Kabupaten Pacitan, Jawa Timur
-                         </li>
-                         <li>
-                              <TicketIcon /> Rp7.000/org
-                         </li>
-                    </ul>
-               </section>
-          </main>
+          <>
+               {data ? (
+                    <main className="bg-purple-200 w-screen p-18 md:p-32 flex flex-col lg:grid grid-cols-2 grid-rows-2 gap-8">
+                         <div className="md:w-full">
+                              <h1 className="font-semibold text-4xl mb-12">{data.title}</h1>
+                              <div className="relative w-full aspect-video rounded-md overflow-hidden">
+                                   <Image src={`/images/destinations/${data.img}`} alt={data.title} fill className="object-cover" priority placeholder="empty" />
+                              </div>
+                         </div>
+                         <article className="flex flex-col gap-3 md:w-full md:mt-20 text-sm md:text-base">
+                              <h3 className="font-semibold text-2xl mb-4">Tentang {data.title}</h3>
+                              <ul className="*:flex flex flex-col md:flex-row bg-purple-900 text-white p-5 rounded-md  gap-4 lg:gap-10  *:gap-2 *:items-start ">
+                                   <li>
+                                        <LocateIcon /> Gumuharjo, Watukarung, Kec. Pringkuku, Kabupaten Pacitan, Jawa Timur
+                                   </li>
+                                   <li>
+                                        <TicketIcon /> Rp7.000/org
+                                   </li>
+                              </ul>
+                              <p>
+                                   Pacitan juga dikenal memiliki gua-gua yang indah, diantaranya Gua Gong, Gua Tabuhan (batu dapat dipukul dan berbunyi seperti alat musik gamelan), Gua Kalak (gua pertapaan), dan Gua Luweng Jaran (diduga sebagai
+                                   kompleks gua terluas di Asia Tenggara). Pacitan juga memiliki banyak pantai indah, seperti Pantai Klayar, Pantai Srau, Pantai Teleng Ria, dan Pantai Kasap. Selain itu, Pacitan juga dikenal sebagai kota kelahiran
+                                   Presiden Indonesia ke-6, Susilo Bambang Yudhoyono.
+                              </p>
+                         </article>
+                         <section className="col-span-2">
+                              <iframe
+                                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15794.507524216038!2d110.97260023301688!3d-8.240221882371037!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7bdde98e5438dd%3A0x61e796fc381a11db!2sPantai%20Kasap!5e0!3m2!1sid!2sid!4v1752130488242!5m2!1sid!2sid"
+                                   className="w-full h-56 lg:h-full rounded-md"
+                                   loading="lazy"
+                                   referrerPolicy="no-referrer-when-downgrade"
+                              ></iframe>
+                         </section>
+                    </main>
+               ) : (
+                    <main className="bg-purple-200 w-screen p-18 md:p-32 flex flex-col lg:grid grid-cols-2 grid-rows-2 gap-8 animate-pulse">
+                         <div className="md:w-full *:rounded-md">
+                              <h1 className="font-semibold text-4xl mb-12 bg-purple-100 py-8"></h1>
+                              <div className="w-full rounded-md aspect-video bg-purple-300"></div>
+                         </div>
+                         <article className="flex flex-col gap-3 md:w-full md:mt-20 text-sm md:text-base *:rounded-md">
+                              <h3 className="font-semibold text-2xl mb-4 py-5 bg-purple-100"></h3>
+                              <ul className="*:flex flex flex-col md:flex-row bg-purple-900 text-white p-5 rounded-md  gap-4 lg:gap-10  *:gap-2 *:items-start py-8"></ul>
+                              <p className="py-25 bg-purple-50"></p>
+                         </article>
+                         <section className="col-span-2 bg-gray-100 rounded-md"></section>
+                    </main>
+               )}
+          </>
      );
 };
 
