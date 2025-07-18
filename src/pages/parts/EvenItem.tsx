ae6e0687e732abd486b2c6d9b9dc28bf432f7836
GithubFarelAlghazali <farelgithub@gmail.com>
2025-07-18 07:37:35 +0700

    move events api to firebase, and adding skeleton

--- src/pages/parts/EvenItem.tsx
+++ src/pages/parts/EvenItem.tsx
@@ -7,16 +7,16 @@ type propsType = {
      img: string;
 };
 
-const EventCard = (props: propsType) => {
-     const { title, desc, img } = props;
+export const EventCard = (props: propsType) => {
+     const { title, desc, img, id } = props;
 
      return (
           <Card className="w-96 p-0 overflow-hidden relative">
                <CardContent className="p-0">
-                    <img src={`/images/events/${img}`} alt="" className="w-full aspect-3/4" />
+                    <img src={`/images/events/${img}`} alt="" className="w-full aspect-3/4" loading="lazy" />
                </CardContent>
                <CardHeader className="pb-5">
-                    <Link href="">
+                    <Link href={`/event/${id}`}>
                          <CardTitle className="text-xl">{title}</CardTitle>
                          <CardDescription>{desc}</CardDescription>
                     </Link>
@@ -25,4 +25,16 @@ const EventCard = (props: propsType) => {
      );
 };
 
-export default EventCard;
+export const EventSkeleton = () => {
+     return (
+          <Card className="w-96 p-0 overflow-hidden relative *:*:rounded-sm animate-pulse">
+               <CardContent className="p-5">
+                    <div className="w-full aspect-3/4 bg-gray-300"></div>
+               </CardContent>
+               <CardHeader className="pb-5  rounded-sm">
+                    <CardTitle className="text-xl bg-gray-500 py-4"></CardTitle>
+                    <CardDescription className="bg-gray-500 py-2"></CardDescription>
+               </CardHeader>
+          </Card>
+     );
+};
--- src/pages/sections/Event/index.tsx
+++ src/pages/sections/Event/index.tsx
@@ -1,38 +1,60 @@
-import EventCard from "@/pages/parts/EvenItem";
+import { EventCard, EventSkeleton } from "@/pages/parts/EvenItem";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
+import { useEffect, useState } from "react";
 
-const data = [
-     {
-          title: "Festival Rontek Pacitan",
-          date: "5 - 7 Juli 2025",
-          location: "Alun-alun Kab. Pacitan",
-          img: "festival-rontek.jpg",
-     },
-     {
-          title: "Festival Nelayan Pacitan",
-          date: "25 Juni 2025",
-          location: "Pelabuhan Tamperan",
-          img: "festival-nelayan.jpg",
-     },
-     {
-          title: "Car Free Day",
-          date: "22 Juni 2025",
-          location: "Alun-alun Kab. Pacitan",
-          img: "cfd.jpg",
-     },
-];
+// const data = [
+//      {
+//           title: "Festival Rontek Pacitan",
+//           date: "5 - 7 Juli 2025",
+//           location: "Alun-alun Kab. Pacitan",
+//           img: "festival-rontek.jpg",
+//      },
+//      {
+//           title: "Festival Nelayan Pacitan",
+//           date: "25 Juni 2025",
+//           location: "Pelabuhan Tamperan",
+//           img: "festival-nelayan.jpg",
+//      },
+//      {
+//           title: "Car Free Day",
+//           date: "22 Juni 2025",
+//           location: "Alun-alun Kab. Pacitan",
+//           img: "cfd.jpg",
+//      },
+// ];
 
 const SectionEvent = () => {
+     const [data, setData] = useState([]);
+
+     useEffect(() => {
+          const url = "http://localhost:3000/api/event";
+          try {
+               fetch(url)
+                    .then((response) => response.json())
+                    .then((data) => {
+                         setData(data.data);
+                    });
+          } catch (error) {
+               console.error("Error fetching event data:", error);
+          }
+     });
+
      return (
           <section className="bg-purple-800 text-white w-screen p-18 md:p-32" id="event">
                <h2 className="font-semibold text-4xl">Event mendatang</h2>
                <Carousel className="w-full mt-10 mx-auto ">
                     <CarouselContent className="">
-                         {data.map((item, index) => (
-                              <CarouselItem key={index} className="w-full flex justify-center items-center basis-full md:basis-1/3">
-                                   <EventCard title={item.title} desc={`${item.date} | ${item.location}`} img={item.img} />
-                              </CarouselItem>
-                         ))}
+                         {data.length === 0
+                              ? Array.from({ length: 3 }).map((_, index) => (
+                                     <CarouselItem key={index} className="w-full flex justify-center items-center basis-full md:basis-1/3">
+                                          <EventSkeleton key={index} />
+                                     </CarouselItem>
+                                ))
+                              : data.map((item, index) => (
+                                     <CarouselItem key={index} className="w-full flex justify-center items-center basis-full md:basis-1/3">
+                                          <EventCard title={item.title} desc={`${item.date} | ${item.location}`} img={item.img} id={item.id} />
+                                     </CarouselItem>
+                                ))}
                     </CarouselContent>
                     <CarouselPrevious className="bg-transparent" />
                     <CarouselNext className="bg-transparent" />
